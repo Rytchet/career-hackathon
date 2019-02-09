621e10bd7911ff044642557a7c49f0b14bdeef8d
Rytchet <oskarino.g@gmail.com>
2019-02-09 11:47:28 +0000

    fix

--- panel/views.py
+++ panel/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi")
+    template = loader.get_template('panel/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
