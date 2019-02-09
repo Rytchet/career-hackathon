a503cfea41868e7ef609718a44b3bb6f5c19c875
Rytchet <oskarino.g@gmail.com>
2019-02-09 11:08:45 +0000

    Add panel app and auth

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
